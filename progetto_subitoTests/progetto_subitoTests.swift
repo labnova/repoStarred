63311f7843613a669c13902c1c526b64469c8f78
Labnova <progettofeb@gmail.com>
2019-10-04 13:15:57 +0200

    Initial Commit

---
+++ progetto_subitoTests/progetto_subitoTests.swift
@@ -0,0 +1,34 @@
+//
+//  progetto_subitoTests.swift
+//  progetto_subitoTests
+//
+//  Created by Innocenzo Tremamondo on 04/10/2019.
+//  Copyright © 2019 Innocenzo Tremamondo. All rights reserved.
+//
+
+import XCTest
+@testable import progetto_subito
+
+class progetto_subitoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
